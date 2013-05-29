1e49ba76342c13e7f2a29c0eb907a42bfd6df3e9
worldsoup <ngottlieb1@gmail.com>
2013-05-28 17:55:02 -0700

    fancy search bar using zepto

--- js/main-ck.js
+++ js/main-ck.js
@@ -1,6 +1,15 @@
-// When ready...
+// scroll away address bar
 window.addEventListener("load", function() {
     setTimeout(function() {
         window.scrollTo(0, 1);
     }, 0);
+});
+
+$("#search-box").focus(function() {
+    $("header").fadeOut;
+});
+
+$("#search-box").blur(function() {
+    $(".search-prompt").show();
+    $("header").show();
 });
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// When ready...
+// scroll away address bar
 window.addEventListener("load",function() {
 	// Set a timeout...
 	setTimeout(function(){
@@ -6,3 +6,44 @@ window.addEventListener("load",function() {
 		window.scrollTo(0, 1);
 	}, 0);
 });
+
+
+$('#search-box') .focus(function(){
+	// $('.search-prompt') .hide()
+	// $('header') .hide()
+	$('header').animate({
+		height: '20px',
+		background: '#231f20',
+	}, 350, 'ease-out')
+	$('#search-box').css('background', '#fff') 
+	$('.search-box-container').append('<div class="x-btn"></div>')
+
+}) 
+
+// $('#search-box') .focus(function(){
+// 	// $('.search-prompt') .hide()
+// 	// $('header') .hide()
+// 	window.scrollTo(0, 3)
+// 	setTimeout(function(){
+// 		$('header').animate({
+// 			height: '0px',
+// 		}, 400, 'ease-out')
+// 	},30)
+// }) 
+$('#search-box') .blur(function(){
+	// $('.search-prompt') .hide()
+	// $('header') .hide()
+	$('header').animate({
+		height: '63px',
+		background: '#fff',
+	}, 350, 'ease-in')
+	var carotImg="../img/carot.png'"
+	$('#search-box').css('background', 'url('+ carotImg +') no-repeat') 
+	$('.x-btn').hide()
+}) 
+
+
+// $('#search-box') .blur(function(){
+// 	$('.search-prompt') .show() 
+// 	$('header') .show()  
+// })   
