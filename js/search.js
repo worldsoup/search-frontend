cd5198eef70e4e0b6f3ede5722bcf244985ddcfa
Raj Sen Sharma <raj@mobozi.com>
2013-06-01 20:17:21 -0700

    authentication cookie integrated

--- js/search.js
+++ js/search.js
@@ -95,7 +95,8 @@ $('#frmLogin').submit(function() {
 			        serverdata = JSON.parse(data);
 			        if (serverdata.status == 201){
 						$('#partiallogin').hide();   
-						$('#partialSearch').show();       
+						$('#partialSearch').show(); 
+						setCookie('searchizobom', 'izobomhcr', 30);      
 				     } else {
 				        $('#errormessage').html('<font color="red">' + serverdata.error + '</font>') 		          
 				    }                       
@@ -106,3 +107,50 @@ $('#frmLogin').submit(function() {
 	});
 return false;
 });
+
+$(function() {
+    checkCookie();
+});
+
+function setCookie(c_name,value,exdays)
+{
+	var exdate=new Date();
+	exdate.setDate(exdate.getDate() + exdays);
+	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+	document.cookie=c_name + "=" + c_value;
+}
+
+function getCookie(c_name)
+{
+	var c_value = document.cookie;
+	var c_start = c_value.indexOf(" " + c_name + "=");
+	if (c_start == -1)
+	 {
+	  	c_start = c_value.indexOf(c_name + "=");
+	 }
+	if (c_start == -1)
+	{
+	  c_value = null;
+	}
+	else
+	{
+	  c_start = c_value.indexOf("=", c_start) + 1;
+	  var c_end = c_value.indexOf(";", c_start);
+	  if (c_end == -1)
+	  {
+		c_end = c_value.length;
+	  }
+	  c_value = unescape(c_value.substring(c_start,c_end));
+	}
+	return c_value;
+}
+
+function checkCookie()
+{
+	var betauser=getCookie("searchizobom");
+  	if (betauser!=null && betauser!="")
+  	{
+  		$('#partiallogin').hide();   
+		$('#partialSearch').show();
+  	}
+}
